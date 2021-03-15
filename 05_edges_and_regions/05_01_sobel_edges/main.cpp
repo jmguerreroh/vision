0ed17dd0230fa0744a4f7e971cafc0156a8a594c
José Miguel Guerrero <josemiguel.guerrero@urjc.es>
2021-03-15 01:30:47 +0100

    First examples chapter 5

---
+++ 05_edges_and_regions/05_01_sobel_edges/main.cpp
@@ -0,0 +1,49 @@
+/**
+ * Sobel edges sample code
+ * @author José Miguel Guerrero
+ */
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <stdio.h>
+using namespace cv;
+using namespace std;
+
+
+
+int main( int argc, char** argv ) {
+    
+    // Read image
+    Mat src = imread("../../images/lenna.jpg", 0); 
+    if(src.empty()) {
+        cout << "the image is not exist" << endl;  
+        return -1;
+    }
+    
+    resize(src, src, Size(512, 512));
+    src.convertTo(src, CV_32F, 1.0/255);
+
+    // Masks
+    Mat SobelGx = (Mat_<char>(3,3) <<   -1, 0, 1,
+                                        -2, 0, 2,
+                                        -1, 0, 1);
+
+
+    Mat SobelGy = (Mat_<char>(3,3) <<   -1, -2, -1,
+                                         0, 0, 0,
+                                         1, 2, 1);
+
+    // Applying masks
+    Mat SobelHorizontal, SobelVertical;
+    filter2D( src, SobelVertical, src.depth(), SobelGx );
+    filter2D( src, SobelHorizontal, src.depth(), SobelGy );
+
+    // Show images
+    imshow("Original", src);
+    imshow("Horizontal edges", SobelHorizontal);
+    imshow("Vertical edges", SobelVertical);
+    waitKey();
+  
+    return 0;
+}
---
+++ 05_edges_and_regions/05_02_canny_edges/main.cpp
@@ -0,0 +1,29 @@
+/**
+ * Canny edges sample code
+ * @author José Miguel Guerrero
+ */
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
+
+int main () {
+    // Create image variables
+    Mat image, edges;
+
+    // Read image
+    image = imread("../../images/lenna.jpg", IMREAD_COLOR);
+
+    // Image processing
+    Canny(image, edges, 0, 100, 3);
+
+    // Show images
+    imshow("TEST IMAGE", image);
+    imshow("CANNY EDGES", edges);
+
+    // Wait to press a key
+    waitKey(0);
+
+    return 0;
+}
