4c39768d98f1be9e2f583907d812207e3a334a94
José Miguel Guerrero <josemiguel.guerrero@urjc.es>
2022-03-10 15:44:31 +0100

    Adding RANSAC,  renaming examples and fixing bugs

--- 02_basic_concepts/02_03_color_space/main.cpp
+++ 02_basic_concepts/02_03_color_space/main.cpp
@@ -30,13 +30,13 @@ int main( int argc, char** argv ) {
  
 
   // Resize images and concatenate them
-  int tam = 4;
+  int tam = 2;
   int h = src.size().height/tam, w = src.size().width/tam;
 
   cv::resize(src,src,Size(h,w),0,0,INTER_LANCZOS4);
-  cv::resize(BGR_channels[0],BGR_channels[0],Size(h,w),0,0,INTER_LANCZOS4);
-  cv::resize(BGR_channels[1],BGR_channels[1],Size(h,w),0,0,INTER_LANCZOS4);
-  cv::resize(BGR_channels[2],BGR_channels[2],Size(h,w),0,0,INTER_LANCZOS4);
+  cv::resize(BGR_channels[0],BGR_channels[0],Size(w,h),0,0,INTER_LANCZOS4);
+  cv::resize(BGR_channels[1],BGR_channels[1],Size(w,h),0,0,INTER_LANCZOS4);
+  cv::resize(BGR_channels[2],BGR_channels[2],Size(w,h),0,0,INTER_LANCZOS4);
 
   cv::Mat win_mat1_rgb(cv::Size(BGR_channels[0].size().height, BGR_channels[0].size().width*2), CV_8UC3);
   cv::Mat win_mat2_rgb(cv::Size(win_mat1_rgb.size().height, (win_mat1_rgb.size().width+BGR_channels[1].size().width)), CV_8UC3);
