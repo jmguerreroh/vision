a3dc2014687829be55512fee6e5bd6487f7d99a9
Jose Miguel <josemiguel.guerrero@urjc.es>
2025-03-04 12:23:58 +0100

    New example
    Signed-off-by: Jose Miguel <josemiguel.guerrero@urjc.es>

--- 05_edges_and_regions/05_01_sobel_edges/main.cpp
+++ 05_edges_and_regions/05_01_sobel_edges/main.cpp
@@ -20,26 +20,53 @@ int main(int argc, char ** argv)
   }
 
   resize(src, src, Size(512, 512));
-  src.convertTo(src, CV_32F, 1.0 / 255);
+
+  // ------------
+  // Method 2:
+  // Use masks to calculate the gradient
 
   // Masks
-  Mat SobelGx = (Mat_<char>(3, 3) <<   -1, 0, 1,
+  Mat SobelGx = (Mat_<char>(3, 3) <<   
+    -1, 0, 1,
     -2, 0, 2,
     -1, 0, 1);
 
-  Mat SobelGy = (Mat_<char>(3, 3) <<   -1, -2, -1,
+  Mat SobelGy = (Mat_<char>(3, 3) <<   
+    -1, -2, -1,
     0, 0, 0,
     1, 2, 1);
 
   // Applying masks
   Mat SobelHorizontal, SobelVertical;
+  Mat grad_masks;
   filter2D(src, SobelVertical, src.depth(), SobelGx);
   filter2D(src, SobelHorizontal, src.depth(), SobelGy);
+  addWeighted(SobelHorizontal, 0.5, SobelVertical, 0.5, 0, grad_masks);
 
   // Show images
   imshow("Original", src);
   imshow("Horizontal edges", SobelHorizontal);
   imshow("Vertical edges", SobelVertical);
+  imshow("Edges", grad_masks);
+
+  // ------------
+  // Method 2:
+  // Use Sobel to calculate the gradient
+  Mat grad_x, grad_y;
+  Mat abs_grad_x, abs_grad_y;
+  Mat grad;
+  Sobel(src, grad_x, src.depth(), 1, 0, 3);
+  Sobel(src, grad_y, src.depth(), 0, 1, 3);
+
+  convertScaleAbs(grad_x, abs_grad_x);
+  convertScaleAbs(grad_y, abs_grad_y);
+  addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+
+  // Show images
+  imshow("Horizontal edges (Sobel)", grad_y);
+  imshow("Vertical edges (Sobel)", grad_x);
+  imshow("Edges (Sobel)", grad);
+
   waitKey();
 
   return 0;
