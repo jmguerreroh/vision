f93dd64a8fdb00595f12795f9880fdb44b462f95
Jose Miguel <josemiguel.guerrero@urjc.es>
2025-03-07 11:40:18 +0100

    Add comments
    Signed-off-by: Jose Miguel <josemiguel.guerrero@urjc.es>

--- 05_edges_and_regions/05_01_sobel_edges/main.cpp
+++ 05_edges_and_regions/05_01_sobel_edges/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char ** argv)
   resize(src, src, Size(512, 512));
 
   // ------------
-  // Method 2:
+  // Method 1:
   // Use masks to calculate the gradient
 
   // Masks
@@ -39,8 +39,10 @@ int main(int argc, char ** argv)
   // Applying masks
   Mat SobelHorizontal, SobelVertical;
   Mat grad_masks;
+  // Gradient X and Y
   filter2D(src, SobelVertical, src.depth(), SobelGx);
   filter2D(src, SobelHorizontal, src.depth(), SobelGy);
+  // Combine both masks
   addWeighted(SobelHorizontal, 0.5, SobelVertical, 0.5, 0, grad_masks);
 
   // Show images
@@ -55,11 +57,13 @@ int main(int argc, char ** argv)
   Mat grad_x, grad_y;
   Mat abs_grad_x, abs_grad_y;
   Mat grad;
+  // Gradient X and Y
   Sobel(src, grad_x, src.depth(), 1, 0, 3);
   Sobel(src, grad_y, src.depth(), 0, 1, 3);
-
+  // Convert to absolute values
   convertScaleAbs(grad_x, abs_grad_x);
   convertScaleAbs(grad_y, abs_grad_y);
+  // Combine both masks
   addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
 
   // Show images
